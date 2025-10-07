84837b500d37ced659bbf38e584cc8e169120548
yuvraj-singh-athwal <58904694+yuvraj-singh-athwal@users.noreply.github.com>
2025-10-06 23:55:01 -0400

    Fixing address

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -11,7 +11,7 @@ function ContactInfo() {
               <div>
                 <h3 className="font-bold">Adresse</h3>
                 <a href="https://www.google.com/maps/place/K+S+Driving+School/@45.5257771,-73.6246395,17z/data=!4m16!1m9!3m8!1s0x4cc9190a30f86c09:0xd480ffd14479dacd!2sK+S+Driving+School!8m2!3d45.5257771!4d-73.6246395!9m1!1b1!16s%2Fg%2F11c5smt26g!3m5!1s0x4cc9190a30f86c09:0xd480ffd14479dacd!8m2!3d45.5257771!4d-73.6246395!16s%2Fg%2F11c5smt26g?entry=ttu&g_ep=EgoyMDI1MTAwMS4wIKXMDSoASAFQAw%3D%3D" target="_blank">
-                  <p>852 Jean Talon Ouest, Suite 200<br />Montreal, QC H3N 1S4</p>
+                  <p>852 Jean Talon Ouest, Suite 209<br />Montreal, QC H3N 1S4</p>
                 </a>
               </div>
             </div>
